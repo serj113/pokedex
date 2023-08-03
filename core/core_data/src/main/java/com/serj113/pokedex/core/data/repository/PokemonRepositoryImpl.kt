a334ef4fc39d6a05cd77511db6a168ebcd088a04
Setia <setia_simaremare@yahoo.com>
2023-08-03 22:01:54 +0700

    fetch pokemon list from remote

--- core/core_data/src/main/java/com/serj113/pokedex/core/data/repository/PokemonRepositoryImpl.kt
+++ core/core_data/src/main/java/com/serj113/pokedex/core/data/repository/PokemonRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.serj113.pokedex.core.data.repository
 import com.serj113.core.domain.repository.PokemonRepository
 import com.serj113.pokedex.core.data.service.PokemonService
 import com.serj113.pokedex.core.model.ApiResult
-import com.serj113.pokedex.core.model.DataItem
 import com.serj113.pokedex.core.model.PokemonListResponse
 import javax.inject.Inject
 
@@ -11,15 +10,17 @@ class PokemonRepositoryImpl @Inject constructor(
   private val service: PokemonService,
 ) : PokemonRepository {
   override suspend fun fetchPokemonList(): ApiResult<PokemonListResponse> {
-    return ApiResult.Success(
-      PokemonListResponse(
-        count = 2,
-        results = listOf(
-          DataItem("Pikachu", ""),
-          DataItem("Pikachu", ""),
-        ),
-      )
-    )
+    return try {
+      val response = service.getPokemonList()
+      val body = response.body()
+      if (body != null && response.isSuccessful) {
+        ApiResult.Success(body)
+      } else {
+        ApiResult.Error()
+      }
+    } catch (e: Exception) {
+      ApiResult.Error(e)
+    }
   }
 
 }
