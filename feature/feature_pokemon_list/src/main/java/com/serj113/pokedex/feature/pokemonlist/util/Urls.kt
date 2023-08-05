524dcd1b015d2842b3f713e4c6109ba5e4219381
Setia <setia_simaremare@yahoo.com>
2023-08-05 13:43:51 +0700

    add sprite image support

---
+++ feature/feature_pokemon_list/src/main/java/com/serj113/pokedex/feature/pokemonlist/util/Urls.kt
@@ -0,0 +1,10 @@
+package com.serj113.pokedex.feature.pokemonlist.util
+
+object Urls {
+  private const val BASE_SPRITE_IMAGE = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+  private const val SPRITE_IMAGE_EXTENSION = ".png"
+
+  fun getSpriteImage(id: String): String {
+    return "$BASE_SPRITE_IMAGE$id$SPRITE_IMAGE_EXTENSION"
+  }
+}
