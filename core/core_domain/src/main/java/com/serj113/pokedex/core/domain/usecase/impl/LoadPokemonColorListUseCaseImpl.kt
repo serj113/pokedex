b3be580a863c06bc410c104b2bd9eb450451bb5e
Setia <setia_simaremare@yahoo.com>
2023-08-17 20:56:57 +0700

    try to add fetch detail loop

--- core/core_domain/src/main/java/com/serj113/pokedex/core/domain/usecase/impl/LoadPokemonColorListUseCaseImpl.kt
+++ core/core_domain/src/main/java/com/serj113/pokedex/core/domain/usecase/impl/LoadPokemonColorListUseCaseImpl.kt
@@ -3,20 +3,33 @@ package com.serj113.pokedex.core.domain.usecase.impl
 import com.serj113.pokedex.core.domain.repository.PokemonRepository
 import com.serj113.pokedex.core.domain.usecase.LoadPokemonColorListUseCase
 import com.serj113.pokedex.core.model.ApiResult
+import com.serj113.pokedex.core.model.PokemonColorDetailResponse
 import javax.inject.Inject
 
 class LoadPokemonColorListUseCaseImpl @Inject constructor(
   private val pokemonRepository: PokemonRepository
 ) : LoadPokemonColorListUseCase {
 
-
+  private var pokemonColorHashMap = hashMapOf<Int, PokemonColorDetailResponse>()
 
   override suspend fun invoke() {
     when (val result = pokemonRepository.fetchPokemonColorList()) {
       is ApiResult.Success -> {
         result.value.results.forEach { pokemonColor ->
-
+          val splits = pokemonColor.url.split("/")
+          fetchDetail(splits[splits.count() - 2].toInt())
         }
+      }
+      is ApiResult.Error -> {
+
+      }
+    }
+  }
+
+  private suspend fun fetchDetail(colorId: Int) {
+    when (val result = pokemonRepository.fetchPokemonDetail(colorId)) {
+      is ApiResult.Success -> {
+        pokemonColorHashMap[colorId] = result.value
       }
       is ApiResult.Error -> {
 
