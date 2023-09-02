26cf846d65b9aa90e9892568ec91e25d8ecf254d
Setia <setia_simaremare@yahoo.com>
2023-09-02 22:53:51 +0700

    add padding to detail card

--- feature/feature_pokemondetail/src/main/java/com/serj113/pokedex/feature/pokemondetail/ui/PokemonDetailContent.kt
+++ feature/feature_pokemondetail/src/main/java/com/serj113/pokedex/feature/pokemondetail/ui/PokemonDetailContent.kt
@@ -1,10 +1,13 @@
 package com.serj113.pokedex.feature.pokemondetail.ui
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.serj113.pokedex.common.presentation.ComposeColor
 import com.serj113.pokedex.core.model.PokemonDetailResponse
 import com.serj113.pokedex.feature.pokemondetail.data.PokemonDetail
@@ -14,6 +17,8 @@ fun PokemonDetailContent(
   viewState: PokemonDetail.ViewState,
 ) {
   Card(
+    modifier = Modifier
+      .padding(start = 16.dp, end = 16.dp),
     colors = CardDefaults.cardColors(
       containerColor = ComposeColor.white,
     )
