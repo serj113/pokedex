49cca914bffa77923871c288012ad7adf9167609
Setia <setia_simaremare@yahoo.com>
2023-09-10 23:38:37 +0700

    add status column to detail

--- feature/feature_pokemondetail/src/main/java/com/serj113/pokedex/feature/pokemondetail/ui/StatusColumn.kt
+++ feature/feature_pokemondetail/src/main/java/com/serj113/pokedex/feature/pokemondetail/ui/StatusColumn.kt
@@ -1,14 +1,22 @@
 package com.serj113.pokedex.feature.pokemondetail.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.serj113.pokedex.feature.pokemondetail.data.PokemonDetail
 
 @Composable
 fun StatusColumn(
   viewState: PokemonDetail.ViewState,
 ) {
-  Column {
+  Column(
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(start = 8.dp, end = 8.dp),
+  ) {
     viewState.pokemonDetail.stats.map { stat ->
       StatusItem(stat = stat)
     }
